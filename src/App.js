90fccd8d9bf41e30500c39ca1e3a8f26aad55b68
JunWei3112 <e0543453@u.nus.edu>
2022-05-17 15:52:23 +0800

    Implement basic template of a task list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div>
+class Task extends React.Component {
+    render() {
+        return (
+            <div className="task">
+                {this.props.task}
+            </div>
+        )
+    }
+}
 
-    </div>
-  );
+class TaskList extends React.Component {
+    render() {
+        const taskList = this.props.tasks.map((task, index) => {
+            const taskDesc = '#' + (index + 1) + ": " + task;
+            return (
+                <div key={index}>
+                    {taskDesc}
+                </div>
+            )
+        })
+
+        return (
+            <div>{taskList}</div>
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+            taskData: '',
+        }
+        this.onHandleChange = this.onHandleChange.bind(this);
+        this.onHandleSubmit = this.onHandleSubmit.bind(this);
+    }
+
+    onHandleChange(taskInput) {
+        this.setState({
+            taskData: taskInput.target.value,
+        })
+    }
+
+    onHandleSubmit(e) {
+        e.preventDefault();
+        const newTasks = this.state.tasks.slice();
+        newTasks.push(this.state.taskData);
+        this.setState({
+            tasks: newTasks,
+            taskData: '',
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <TaskList tasks={this.state.tasks}></TaskList>
+                <form>
+                    <input 
+                        type="text" 
+                        onChange={this.onHandleChange}
+                        placeholder="Add A New Task?"
+                        value={this.state.taskData}
+                    />
+                    <button onClick={this.onHandleSubmit} type="submit">
+                        Add Task
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default App;
